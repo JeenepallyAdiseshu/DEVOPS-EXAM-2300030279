c2abaf24e0c17f03ee693513ad73471390bbcd29
Adiseshu <jeenepallyadiseshu@gmail.com>
2025-10-24 18:22:55 +0530

    project'

--- BACKEND/src/main/java/com/klef/devops/BackendApplication.java
+++ BACKEND/src/main/java/com/klef/devops/BackendApplication.java
@@ -2,15 +2,19 @@ package com.klef.devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-
 @SpringBootApplication
-public class BackendApplication extends   SpringBootServletInitializer{
+public class BackendApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-		System.out.println("BACKEND is Running");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+        System.out.println("BACKEND is Running");
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BackendApplication.class);
+    }
 }
